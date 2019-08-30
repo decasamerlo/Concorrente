35844d563894a5aa7e2fda8db00ee1769d6be54e
Andre Merlo <casagrandamerlo@gmail.com>
2019-08-30 13:38:40 -0300

    resolvido exercicio_1 da atividade_2

--- atividade_2/exercicio_1/main.c
+++ atividade_2/exercicio_1/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
+#define N_PIDS 2
 
-//       (pai)      
-//         |        
+//       (pai)
+//         |
 //    +----+----+
-//    |         |   
+//    |         |
 // filho_1   filho_2
 
 
@@ -16,11 +20,22 @@
 // Obs:
 // - pai deve esperar pelos filhos antes de terminar!
 
-
 int main(int argc, char** argv) {
+    int status;
+    pid_t pid_filhos[N_PIDS];
 
-    // ....
+    for (int i = 0; i < N_PIDS; i++) {
+      fflush(stdin);
+      pid_filhos[i] = fork();
+      if (pid_filhos[i]) {
+        printf("Processo pai criou %d\n", pid_filhos[i]);
+      } else {
+        printf("Processo filho %d criado\n", getpid());
+        exit(1);
+      }
+    }
 
-    printf("Processo pai finalizado!\n");   
+    wait(&status);
+    printf("Processo pai finalizado!\n");
     return 0;
 }
