74e44093f993a38dbec2ff4dde1b6fb22aacf526
Andre Merlo <casagrandamerlo@gmail.com>
2019-09-01 02:11:35 -0300

    resolvido exercicio_2 da atividade_2

--- atividade_2/exercicio_2/main.c
+++ atividade_2/exercicio_2/main.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define N_FILHOS 2
+#define N_NETOS 3
 
 //                          (principal)
 //                               |
@@ -20,9 +26,36 @@
 // - pais devem esperar pelos seu descendentes diretos antes de terminar
 
 int main(int argc, char** argv) {
-    
-    // ....
+  int status;
 
-    printf("Processo principal %d finalizado\n", getpid());    
-    return 0;
+  pid_t pid_filhos[N_FILHOS];
+  pid_t pid_netos[N_NETOS];
+
+  for (int i = 0; i < N_FILHOS; i++) {
+    fflush(stdout);
+    pid_filhos[i] = fork();
+    if (pid_filhos[i]) {
+    } else {
+      printf("Processo %d, filho de %d\n", getpid(), getppid());
+      for (int i = 0; i < N_NETOS; i++) {
+        fflush(stdout);
+        pid_netos[i] = fork();
+        if (pid_netos[i]) {
+        } else {
+          printf("Processo %d, filho de %d\n", getpid(), getppid());
+          sleep(5);
+          printf("Processo %d finalizado\n", getpid());
+          exit(1);
+        }
+      }
+      waitpid(0,&status,WUNTRACED);
+      printf("Processo %d finalizado\n", getpid());
+      exit(1);
+    }
+  }
+
+  waitpid(0,&status,WUNTRACED);
+  printf("Processo principal %d finalizado\n", getpid());
+
+  return 0;
 }
