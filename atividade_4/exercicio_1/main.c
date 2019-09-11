9c25f12664263f877af52d1da01557366bfdbb59
Andre Merlo <casagrandamerlo@gmail.com>
2019-09-11 13:46:03 -0300

    resolvido exercicio_1 da atividade_4

--- atividade_4/exercicio_1/main.c
+++ atividade_4/exercicio_1/main.c
@@ -25,12 +25,15 @@
 
 
 int contador_global = 0;
+pthread_mutex_t mutex;
 
 
 void *incrementor(void *arg) {
     int n_loops = *(int *)arg;
     for (int i = 0; i < n_loops; i++) {
+        pthread_mutex_lock(&mutex);
         contador_global += 1;
+        pthread_mutex_unlock(&mutex);
     }
     pthread_exit(NULL); // ou return NULL;
 }
@@ -46,12 +49,14 @@ int main(int argc, char* argv[]) {
     int n_loops = atoi(argv[2]);
     pthread_t threads[n_threads];
 
+    pthread_mutex_init(&mutex, NULL);
     for (int i = 0; i < n_threads; i++)
         pthread_create(&threads[i], NULL, incrementor, (void*)&n_loops);
 
     for (int i = 0; i < n_threads; i++)
         pthread_join(threads[i], NULL);
     
+    pthread_mutex_destroy(&mutex);
     printf("Contador: %d\n", contador_global);
     printf("Esperado: %d\n", n_threads * n_loops);
     return 0;
