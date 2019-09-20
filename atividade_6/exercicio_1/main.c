8345c14b83f56b961f0b04a4ee6ec6ae3640a7a8
Andre Merlo <casagrandamerlo@gmail.com>
2019-09-20 10:58:51 -0300

    resolvido exercicio_1 da atividade_6

--- atividade_6/exercicio_1/main.c
+++ atividade_6/exercicio_1/main.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/time.h>
 
 #define WORKER_LOOPS 20
 
@@ -17,24 +18,26 @@ int total_computado;
 int *lista_de_operacoes;
 int proximo_indice;
 
-pthread_mutex_t mutex_total;
+pthread_mutex_t mutex_total;//, mutex_operacao;
 sem_t sem_lista;
 
 void *worker1_func(void *arg) {
     for (int i = 0; i < WORKER_LOOPS; ++i) {
-        sem_wait(&sem_lista);
-        printf("Worker 1 obteve sem_lista\n");
-        int operacao = operacao_worker1();
         pthread_mutex_lock(&mutex_total);
         printf("Worker 1 obteve mutex_total\n");
+        //pthread_mutex_lock(&mutex_operacao);
+        int operacao = operacao_worker1();
+        //pthread_mutex_unlock(&mutex_operacao);
 
         total_computado += operacao;
-        lista_de_operacoes[proximo_indice++] = operacao;
-
-        sem_post(&sem_lista);
-        printf("Worker 1 liberou sem_lista\n");
         pthread_mutex_unlock(&mutex_total);
         printf("Worker 1 liberou mutex_total\n");
+
+        sem_wait(&sem_lista);
+        printf("Worker 1 obteve sem_lista\n");
+        lista_de_operacoes[proximo_indice++] = operacao;
+        sem_post(&sem_lista);
+        printf("Worker 1 liberou sem_lista\n");
     }
    
     return NULL;
@@ -44,16 +47,17 @@ void *worker2_func(void *arg) {
     for (int i = 0; i < WORKER_LOOPS; ++i) {
         pthread_mutex_lock(&mutex_total);
         printf("Worker 2 obteve mutex_total\n");
-        sem_wait(&sem_lista);
-        printf("Worker 2 obteve sem_lista\n");
+        //pthread_mutex_lock(&mutex_operacao);
         int operacao = operacao_worker2();
+        //pthread_mutex_unlock(&mutex_operacao);
 
-        lista_de_operacoes[proximo_indice] = operacao;
-        total_computado += lista_de_operacoes[proximo_indice];
-        proximo_indice += 1;
-
+        total_computado += operacao;
         pthread_mutex_unlock(&mutex_total);
         printf("Worker 2 liberou mutex_total\n");
+
+        sem_wait(&sem_lista);
+        printf("Worker 2 obteve sem_lista\n");
+        lista_de_operacoes[proximo_indice++] = operacao;
         sem_post(&sem_lista);
         printf("Worker 2 liberou sem_lista\n");
     }
@@ -62,6 +66,8 @@ void *worker2_func(void *arg) {
 
 
 int main(int argc, char *argv[]) {
+    struct timeval begin, end;
+    gettimeofday(&begin, NULL);
     //Inicia as variáveis globais
     proximo_indice = 0;
     lista_de_operacoes = malloc(sizeof(int) * 2*WORKER_LOOPS);
@@ -70,6 +76,7 @@ int main(int argc, char *argv[]) {
     //Inicia semáforos e mutexes
     sem_init(&sem_lista, 0, 1);
     pthread_mutex_init(&mutex_total, NULL);
+    //pthread_mutex_init(&mutex_operacao, NULL);
 
     //Cria as threads do worker1 e worker2.
     pthread_t worker1, worker2;
@@ -85,7 +92,12 @@ int main(int argc, char *argv[]) {
     //Libera mutexes, semáforos e memória alocada
     sem_destroy(&sem_lista);
     pthread_mutex_destroy(&mutex_total);
+    //pthread_mutex_destroy(&mutex_operacao);
     free(lista_de_operacoes);
 
+    gettimeofday(&end, NULL);
+    double elapsed = (end.tv_sec - begin.tv_sec) +
+    ((end.tv_usec - begin.tv_usec)/1000000.0);
+    printf("time = %f\n", elapsed);
     return 0;
 }
