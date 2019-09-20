2f03d1161b8fef76d6d28a2d9efb425b57b53912
Andre Merlo <casagrandamerlo@gmail.com>
2019-09-20 10:35:04 -0300

    resolvido exercicio_2 da atividade_5

--- atividade_5/exercicio_2/main.c
+++ atividade_5/exercicio_2/main.c
@@ -14,7 +14,8 @@ void *consumidor_func(void *arg);
 int indice_produtor, indice_consumidor, tamanho_buffer;
 int* buffer;
 
-sem_t semaforo_produtor, semaforo_consumidor, semaforo_buffer;
+sem_t semaforo_produtor, semaforo_consumidor;
+pthread_mutex_t consumidor_mtx, produtor_mtx;
 
 //Você deve fazer as alterações necessárias nesta função e na função
 //consumidor_func para que usem semáforos para coordenar a produção
@@ -22,46 +23,32 @@ sem_t semaforo_produtor, semaforo_consumidor, semaforo_buffer;
 void *produtor_func(void *arg) {
     //arg contem o número de itens a serem produzidos
     int max = *((int*)arg);
-    for (int i = 0; i <= max; ++i) {
-        int produto;
-        // printf("Produziu %d\n", produto);
-        if (i == max) {
-            produto = -1;          //envia produto sinlizando FIM
-            // sem_post(&semaforo_produtor);
-        }
-        else 
-            produto = produzir(i); //produz um elemento normal
+    for (int i = 0; i < max; ++i) {
+        int produto = produzir(i); //produz um elemento normal
         sem_wait(&semaforo_produtor);
-        sem_wait(&semaforo_buffer);
+        pthread_mutex_lock(&produtor_mtx); //seção crítica entre produtores
         indice_produtor = (indice_produtor + 1) % tamanho_buffer; //calcula posição próximo elemento
         buffer[indice_produtor] = produto; //adiciona o elemento produzido à lista
-        sem_post(&semaforo_buffer);
+        pthread_mutex_unlock(&produtor_mtx);
         sem_post(&semaforo_consumidor);
     }
-    // sem_post(&semaforo_produtor);
-    // sem_post(&semaforo_consumidor);
     return NULL;
 }
 
 void *consumidor_func(void *arg) {
     while (1) {
-        // printf("Vai consumir \n");
         sem_wait(&semaforo_consumidor);
-        sem_wait(&semaforo_buffer);
+        pthread_mutex_lock(&consumidor_mtx); // Seção crítica entre consumidores
         indice_consumidor = (indice_consumidor + 1) % tamanho_buffer; //Calcula o próximo item a consumir
         int produto = buffer[indice_consumidor]; //obtém o item da lista
-        sem_post(&semaforo_buffer);
+        pthread_mutex_unlock(&consumidor_mtx);
         sem_post(&semaforo_produtor);
         //Podemos receber um produto normal ou um produto especial
         if (produto >= 0)
             consumir(produto); //Consome o item obtido.
-        else {
-            // sem_post(&semaforo_consumidor);
+        else
             break; //produto < 0 é um sinal de que o consumidor deve parar
-        }
     }
-    // sem_post(&semaforo_consumidor);
-    // sem_post(&semaforo_produtor);
     return NULL;
 }
 
@@ -89,7 +76,8 @@ int main(int argc, char *argv[]) {
     // threads criem cada uma n_itens produtos e o n_consumidores os
     // consumam.
 
-    sem_init(&semaforo_buffer, 0, 1);
+    pthread_mutex_init(&produtor_mtx, NULL);
+    pthread_mutex_init(&consumidor_mtx, NULL);
     sem_init(&semaforo_produtor, 0, tamanho_buffer);
     sem_init(&semaforo_consumidor, 0, 0);
 
@@ -102,19 +90,21 @@ int main(int argc, char *argv[]) {
     }
 
     for(int i=0; i < n_produtores; i++) {
-        // sem_post(&semaforo_produtor);
         pthread_join(produtores[i], NULL);
     }
 
-    printf("terminou de produzir");
+    for (int i = 0; i < n_consumidores; ++i) {
+        sem_wait(&semaforo_produtor);
+        buffer[indice_produtor = (indice_produtor+1) % tamanho_buffer] = -1;
+        sem_post(&semaforo_consumidor);
+    }
 
-        sem_post(&semaforo_consumidor);
     for(int i=0; i < n_consumidores; i++) {
-        sem_post(&semaforo_consumidor);
         pthread_join(consumidores[i], NULL);
     }
     
-    sem_destroy(&semaforo_buffer);
+    pthread_mutex_destroy(&produtor_mtx);
+    pthread_mutex_destroy(&consumidor_mtx);
     sem_destroy(&semaforo_produtor);
     sem_destroy(&semaforo_consumidor);
     
