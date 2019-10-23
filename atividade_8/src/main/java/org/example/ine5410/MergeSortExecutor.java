7a636c36822f07f096df89ffe2fbcfae49b47036
Andre Merlo <casagrandamerlo@gmail.com>
2019-10-23 15:02:31 -0300

    resolvido passo_2 da atividade_8

--- atividade_8/src/main/java/org/example/ine5410/MergeSortExecutor.java
+++ atividade_8/src/main/java/org/example/ine5410/MergeSortExecutor.java
@@ -9,12 +9,12 @@ public class MergeSortExecutor<T extends Comparable<T>> implements MergeSort<T> 
     @Nonnull
     @Override
     public ArrayList<T> sort(@Nonnull List<T> list) {
-        //1. Crie um Cached ExecutorService
+        // 1. Crie um Cached ExecutorService
         // (Executors Single thread ou fixed thread pool) causarão starvation!
-        //2. Submete uma tarefa incial ao executor
-        //3. Essa tarefa inicial vai se subdividir em novas tarefas enviadas para
-        //   o mesmo executor
-        //4. Desligue o executor ao sair!
+        // 2. Submete uma tarefa incial ao executor
+        // 3. Essa tarefa inicial vai se subdividir em novas tarefas enviadas para
+        // o mesmo executor
+        // 4. Desligue o executor ao sair!
 
         if (list.size() <= 1)
             return new ArrayList<>(list);
@@ -25,7 +25,21 @@ public class MergeSortExecutor<T extends Comparable<T>> implements MergeSort<T> 
         int mid = list.size() / 2;
         ArrayList<T> left = null;
         /* ~~~~ Execute essa linha paralelamente! ~~~~ */
-        //left = sort(list.subList(0, mid));
+        
+        Future<ArrayList<T>> future = executor.submit(new Callable<ArrayList<T>>() {
+
+            @Override
+            public ArrayList<T> call() throws Exception {
+                return sort(list.subList(0, mid));
+            }
+
+        });
+        executor.shutdown();
+        try {
+            left = future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
         ArrayList<T> right = sort(list.subList(mid, list.size()));
 
         return MergeSortHelper.merge(left, right);
