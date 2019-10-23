b8c5044c59a059cddddb5969852d90a645926658
Andre Merlo <casagrandamerlo@gmail.com>
2019-10-23 15:02:02 -0300

    resolvido passo_1 da atividade_8

--- atividade_8/src/main/java/org/example/ine5410/MergeSortThread.java
+++ atividade_8/src/main/java/org/example/ine5410/MergeSortThread.java
@@ -10,6 +10,8 @@ public class MergeSortThread<T extends Comparable<T>> implements MergeSort<T> {
     public ArrayList<T> sort(@Nonnull final List<T> list) {
         //1. Há duas sub-tarefas, execute-as em paralelo usando threads
         //  (Para pegar um retorno da thread filha faça ela escrever em um ArrayList)
+        final ArrayList<ArrayList<T>> results = new ArrayList<>();
+        results.add(null);
 
         if (list.size() <= 1)
             return new ArrayList<>(list);
@@ -17,9 +19,19 @@ public class MergeSortThread<T extends Comparable<T>> implements MergeSort<T> {
         int mid = list.size() / 2;
         ArrayList<T> left = null;
         /* ~~~~ Execute essa linha paralelamente! ~~~~ */
-        //left = sort(list.subList(0, mid));
-        ArrayList<T> right = sort(list.subList(mid, list.size()));
+        
+        Thread d = new Thread(() -> {
+            results.set(0, sort(list.subList(0, mid)));
+        });
+        d.start();
 
+        try {
+            d.join();
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+        left = results.get(0);
+        ArrayList<T> right = sort(list.subList(mid, list.size()));
         return MergeSortHelper.merge(left, right);
     }
 }
