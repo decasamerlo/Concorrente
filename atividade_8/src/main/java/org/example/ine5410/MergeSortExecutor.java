cb2ec62736daf5e0f0281d420b6b2e2cf9f3f409
Andre Merlo <casagrandamerlo@gmail.com>
2019-10-24 16:24:49 -0300

    resolvido passo_3 da atividade_8

--- atividade_8/src/main/java/org/example/ine5410/MergeSortExecutor.java
+++ atividade_8/src/main/java/org/example/ine5410/MergeSortExecutor.java
@@ -6,6 +6,9 @@ import java.util.List;
 import java.util.concurrent.*;
 
 public class MergeSortExecutor<T extends Comparable<T>> implements MergeSort<T> {
+
+    ThreadGroup tg = new ThreadGroup("groupExecutor");
+
     @Nonnull
     @Override
     public ArrayList<T> sort(@Nonnull List<T> list) {
@@ -20,28 +23,39 @@ public class MergeSortExecutor<T extends Comparable<T>> implements MergeSort<T> 
             return new ArrayList<>(list);
 
         /* ~~~~ O tipo do executor precisa ser Cached!!!! ~~~~ */
-        ExecutorService executor = Executors.newCachedThreadPool();
+        ExecutorService executor = Executors.newCachedThreadPool(new ThreadFactory(){
+        
+            @Override
+            public Thread newThread(Runnable arg0) {
+                return new Thread(tg, arg0);
+            }
+        });
 
         int mid = list.size() / 2;
         ArrayList<T> left = null;
         /* ~~~~ Execute essa linha paralelamente! ~~~~ */
-        
-        Future<ArrayList<T>> future = executor.submit(new Callable<ArrayList<T>>() {
 
-            @Override
-            public ArrayList<T> call() throws Exception {
-                return sort(list.subList(0, mid));
+        if (Thread.currentThread().getThreadGroup() == tg) {
+            // System.out.println("created thread with activeCount() = " + Thread.activeCount());
+            Future<ArrayList<T>> future = executor.submit(new Callable<ArrayList<T>>() {
+
+                @Override
+                public ArrayList<T> call() throws Exception {
+                    return sort(list.subList(0, mid));
+                }
+
+            });
+            try {
+                left = future.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
             }
-
-        });
-        executor.shutdown();
-        try {
-            left = future.get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
+        } else {
+            left = sort(list.subList(0, mid));
         }
         ArrayList<T> right = sort(list.subList(mid, list.size()));
 
+        executor.shutdown();
         return MergeSortHelper.merge(left, right);
     }
 }
--- atividade_8/src/main/java/org/example/ine5410/MergeSortThread.java
+++ atividade_8/src/main/java/org/example/ine5410/MergeSortThread.java
@@ -5,11 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MergeSortThread<T extends Comparable<T>> implements MergeSort<T> {
+
+    // private int threadCounter = 1;
+    private ThreadGroup tg = new ThreadGroup("groupThreads");
+
     @Nonnull
     @Override
     public ArrayList<T> sort(@Nonnull final List<T> list) {
-        //1. Há duas sub-tarefas, execute-as em paralelo usando threads
-        //  (Para pegar um retorno da thread filha faça ela escrever em um ArrayList)
+        // 1. Há duas sub-tarefas, execute-as em paralelo usando threads
+        // (Para pegar um retorno da thread filha faça ela escrever em um ArrayList)
         final ArrayList<ArrayList<T>> results = new ArrayList<>();
         results.add(null);
 
@@ -19,18 +23,27 @@ public class MergeSortThread<T extends Comparable<T>> implements MergeSort<T> {
         int mid = list.size() / 2;
         ArrayList<T> left = null;
         /* ~~~~ Execute essa linha paralelamente! ~~~~ */
-        
-        Thread d = new Thread(() -> {
-            results.set(0, sort(list.subList(0, mid)));
-        });
-        d.start();
+        if (Thread.currentThread().getThreadGroup() != tg) { // muito lento, chega mais próximo
+        // if (list.size() == Thread.activeCount()) { // -> não roda concorrentemente
+        // if (list.size() == 3) { // -> muito lento, chega próximo
+        // if (list.size() % 2 == 0) { // -> muito lento
+        // if (list.size() == Thread.getAllStackTraces().size()) { // -> muito lento 
+            // threadCounter++;
+            // System.out.println("ThreadCounter: " + threadCounter);
+            Thread d = new Thread(tg, () -> {
+                results.set(0, sort(list.subList(0, mid)));
+            });
+            d.start();
 
-        try {
-            d.join();
-        } catch(InterruptedException e) {
-            e.printStackTrace();
+            try {
+                d.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            left = results.get(0);
+        } else {
+            left = sort(list.subList(0, mid));
         }
-        left = results.get(0);
         ArrayList<T> right = sort(list.subList(mid, list.size()));
         return MergeSortHelper.merge(left, right);
     }
