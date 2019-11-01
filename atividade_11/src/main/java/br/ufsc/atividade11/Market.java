e5d1c7fd63b7d8cb41b733c1238148f28fe7ba83
Andre Merlo <casagrandamerlo@gmail.com>
2019-11-01 11:32:57 -0300

    resolvida atividade_11

--- atividade_11/src/main/java/br/ufsc/atividade11/Market.java
+++ atividade_11/src/main/java/br/ufsc/atividade11/Market.java
@@ -9,32 +9,51 @@ import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Market {
-    private Map<Product, Double> prices = new HashMap<>();
+	private Map<Product, Double> prices = new HashMap<>();
+	private Map<Product, ReadWriteLock> locks = new HashMap<>();
+	private Map<Product, Condition> conditions = new HashMap<>();
 
-    public Market() {
-        for (Product product : Product.values()) {
-            prices.put(product, 1.99);
-        }
-    }
+	public Market() {
+		for (Product product : Product.values()) {
+			prices.put(product, 1.99);
+			locks.put(product, new ReentrantReadWriteLock());
+			conditions.put(product, locks.get(product).writeLock().newCondition());
+		}
+	}
 
-    public void setPrice(@Nonnull Product product, double value) {
-        prices.put(product, value);
-    }
+	public void setPrice(@Nonnull Product product, double value) {
+		locks.get(product).writeLock().lock();
+		prices.put(product, value);
+		conditions.get(product).signalAll();
+		locks.get(product).writeLock().unlock();
+	}
 
-    public double take(@Nonnull Product product) {
-        return prices.get(product);
-    }
+	public double take(@Nonnull Product product) {
+		locks.get(product).readLock().lock();
+		return prices.get(product);
+	}
 
-    public void putBack(@Nonnull Product product) {
-    }
+	public void putBack(@Nonnull Product product) {
+		locks.get(product).readLock().unlock();
+	}
 
-    public double waitForOffer(@Nonnull Product product,
-                               double maximumValue) throws InterruptedException {
-        //deveria esperar até que prices.get(product) <= maximumValue
-        return prices.get(product);
-    }
+	public double waitForOffer(@Nonnull Product product, double maximumValue) throws InterruptedException {
+		locks.get(product).writeLock().lock();
+		try {
+			while (prices.get(product) > maximumValue) {
+				conditions.get(product).await();
+			}
+			locks.get(product).readLock().lock();
+		} finally {
+			locks.get(product).writeLock().unlock();
+		}
+		// deveria esperar até que prices.get(product) <= maximumValue
+		return prices.get(product);
+	}
 
-    public double pay(@Nonnull Product product) {
-        return prices.get(product);
-    }
+	public double pay(@Nonnull Product product) {
+		double price = prices.get(product);
+		locks.get(product).readLock().unlock();
+		return price;
+	}
 }
